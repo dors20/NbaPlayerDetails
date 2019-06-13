445a5697fc776d77b90d7498199d2d98e574c61b
maheshd20 <50525616+maheshd20@users.noreply.github.com>
2019-06-13 21:14:48 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,20 @@
+import json
+
+myplayer=json.load(open("nbaplayer.json"))
+
+print(type(myplayer))
+
+print( myplayer[0]['firstName'].lower()+' '+myplayer[0]['lastName'].lower())
+def PlayerSearch(name):
+    i=1
+    search=0
+    for k in range(0,len(myplayer)):
+        if( (myplayer[k]['firstName'].lower()+' '+myplayer[k]['lastName'].lower())==name.lower()):
+            print(myplayer[k])
+            search=1
+    if(search==0):
+        print("invalid entery...PLease try again")
+
+playsearch=input("enter player name")
+
+print(PlayerSearch(playsearch))
